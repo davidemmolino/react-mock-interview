08afd1b72afeee0820a66c90dfc6fab6437f38fa
davidemmolino <davidewebdev@gmail.com>
2020-11-30 10:48:23 -0800

    finished step 4

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-## Prompt 
+## Instructions 
 
 1. Build a counter feature. Don't worry about styling, just make a button and each time that you click this button a counter goes up by one.
 
@@ -40,4 +40,6 @@ Either have it load on mount or have it load after a click of a button.
 (Try it out using async/await ~ try/catch syntax)
 (Best practice would be to put fetch logic in an external file and import it, try it out after you've finalized step 2).
 
-3. Display fetch results as some UI component (just one result for now), please display the username and picture. Style does not matter, have fun with it or not. 
+3. Display fetch results as some UI component (just one result for now), display the username and picture. Style does not matter, have fun with it or not. 
+
+4. Now that you're able to display one user, how would you go about displaying multiple users? 
--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
@@ -7,30 +6,47 @@ function App() {
 
   const [ counter, setCounter ] = useState(0);
   const [ userData, setUserData ] = useState('');
+  const [ userInfo, setUserInfo ] = useState([]);
+  const [ nextPage, setNextPage ] = useState(1);
 
   const increaseCounter = () => {
     setCounter(counter + 1)
   };
 
-  const queryString = 'https://randomuser.me/api';
-
+  const fetchNextUser = () => {
+    fetchRandomData(nextPage).then(randomData => {
+      if (randomData === undefined) return;
+      const newUserInfo = [
+        ...userInfo, 
+        ...randomData.results 
+      ];
+      setUserInfo(newUserInfo);
+      setNextPage(randomData.info.page + 1);
+    });
+  }
   // fetch data on button click
-  const fetchRandomData = async () => {
+  const fetchRandomData = async (number = nextPage) => {
     try { 
       // Make a request 
-      const data = await axios.get(queryString)
-      console.log(data);
-      setUserData(JSON.stringify(data, null, 2));
+      const {data} = await axios.get(`https://randomuser.me/api?page=${number}`)
+      // setUserData(JSON.stringify(data, null, 2));
+      return data;
     } catch (err) {
       console.log(err)
     }
   };
 
+  // constructor function to get full name of user
+  const getFullName = (user) => {
+    const { name: { first, last}} = user;
+    return `${first} ${last}`;
+  };
+
   // fetch data on page load
   // mimic componentDidMount
-  // useEffect(() => {
-  //   fetchRandomData();
-  // }, []);
+  useEffect(() => {
+    fetchNextUser();
+  }, []);
 
   return (
     <div>
@@ -38,7 +54,14 @@ function App() {
       { counter }
       <br/>
       <button onClick={() => fetchRandomData()}>Fetch Random User Data</button>
+      <button onClick={ fetchNextUser }>Get next user</button>
       <p>{ userData }</p>
+      { userInfo.map((user, i) => (
+        <div key={`${user}_${i}`}>
+            <p>{getFullName(user)}</p>
+            <img src={`${user.picture.thumbnail}`} alt={`${user.first}'s profile`} />
+        </div>
+      )) }
     </div>
   );
 }
